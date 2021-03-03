2a4c6fb4ece09d698115f77ab68d1a4c89afec8e
Tuna ÖZKAN <75748254+tunazkn@users.noreply.github.com>
2021-03-03 16:40:14 +0300

    Add project files.

---
+++ DataAccess/Concrate/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrate.InMemory
+{
+    //GetById, GetAll, Add, Update, Delete
+    class InMemoryCarDal
+    {
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public short ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
